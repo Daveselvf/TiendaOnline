a24386959fa1bbba557ce2ae7464b2bdce718dca
Daveselvf <programacionfronts@gmail.com>
2023-05-30 15:55:35 -0400

    clase 14

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'articulosclientes',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '192.168.56.106',
+        'DATABASE_PORT': '5432',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
